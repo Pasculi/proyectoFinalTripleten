939f485893c74a7cf57c48397ca79dd9f14df0c7
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-10-05 08:05:18 -0300

    feat:Implementación de top-rate y upcomming

---
+++ src/components/TopRate/TopRate.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import "./TopRate.css";
+import Skeletons from "../Skeletons/Skeletons";
+import api from "../../utils/ThirdPartyApi";
+import Card from "../Card/Card";
+import Pagination from "../Pagination/Pagination";
+
+const TopRate = ({ limitMovies, loading, mostrarMas }) => {
+  
+  const [searchMovie, setSearchMovie] = useState([]);
+
+  const searchMovies = () => {
+    api.getTopRate().then((response) => {
+      setSearchMovie(response.results);
+    });
+  };
+
+  useEffect(() => {
+    searchMovies();
+  }, []);
+
+  const dinamicArray = Array.from(Array(limitMovies).keys());
+
+  return (
+    <>
+      <div className="main" id="header-start">
+        <div className="main-container">
+          {loading
+            ? dinamicArray.map((items, index) => {
+                return <Skeletons key={index} />;
+              })
+            : searchMovie?.slice(0, limitMovies).map((movie, index) => {
+                return (
+                  <Card
+                    key={index}
+                    id={movie.id}
+                    title={movie.title}
+                    vote={movie.vote_average}
+                    poster={movie.poster_path}
+                  />
+                );
+              })}
+        </div>
+      </div>
+      {searchMovie?.length >= 0 ? (
+        <Pagination
+          limitMovies={limitMovies}
+          mostrarMas={mostrarMas}
+       
+        />
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+
+export default TopRate;
