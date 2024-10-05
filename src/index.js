938359246466b5c14f0ba2bf743f633ad9c3e9ed
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-10-05 00:27:39 -0300

    feat:Creación de carpetas de archivos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CssBaseline />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
